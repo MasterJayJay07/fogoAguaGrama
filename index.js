d775ff6d687e989ed2426a6b6c37e30ca4f92474
Johão Pedro <johaopedro99@gmail.com>
2021-11-24 20:38:21 -0300

    funções feitas pelo Lucas e Guerreiro de validar uma vitória foi unida com a função do Alexandre de mostrar algo na tela dependendo do resultado

--- index.js
+++ index.js
@@ -1,47 +1,9 @@
-//Fazer a máquina escolher um valor aleatório e atribuir a uma variável.
-
-const arrElementos = ['Grama', 'Àgua', 'Fogo'];
-let elementoMaquina = "";
-
-function gerarElementoMaquina(){
-    elementoMaquina = arrElementos[gerarNumeroRandomico()];
-    console.log(elementoMaquina)
-    return elementoMaquina;
-    
-}
-
-function gerarNumeroRandomico(){
-    return Math.floor(Math.random() * 3);
-}
-
-//Criar botão de jogar para chamar a o geradorElementoMaquina.
-const main = document.getElementById('main');
-const button = document.createElement('button');
-button.innerText = 'Jogar'
-button.addEventListener('click', gerarElementoMaquina);
-main.appendChild(button);
-
-
-//Css do Projeto, gerar div ou section com as opções escritas: vitória, derrota ou empate;
-function telaDeResutado(valor){
-    if(valor === 'vitoria'){
-        const vit = document.getElementById('vitoria');
-        vit.classList.add('display')
-    }
-    if(valor === 'derrota'){
-        const vit = document.getElementById('derrota');
-        vit.classList.add('display')
-    }
-    if(valor === 'empate'){
-        const vit = document.getElementById('empate');
-        vit.classList.add('display')
-    }
-}
-
-//Função pra dar reload na pagina e começar a jogar novamente.
+const mensagens = document.getElementsByClassName("btns")
 
 function reload(){
-    return document.location.reload(true);
+    for(let i=0; i<mensagens.length; i++){
+        mensagens[i].classList.add("display")
+    }
 }
 
 const buttonReloadVitoria = document.getElementById('reload-vitoria');
@@ -50,5 +12,3 @@ const buttonReloadEmpate = document.getElementById('reload-empate');
 buttonReloadVitoria.addEventListener('click', reload);
 buttonReloadDerrota.addEventListener('click', reload);
 buttonReloadEmpate.addEventListener('click', reload);
-
-
---
+++ script.js
@@ -0,0 +1,102 @@
+const buttons = document.getElementsByClassName('btn')
+const main = document.getElementById('main');
+const button = document.createElement('button');
+button.innerText = 'Jogar'
+main.appendChild(button);
+
+//variável que será usada para validar com a escolha da máquina
+let escolhaAtual = ""
+let elementoMaquina = "";
+
+const arrElementos = ['Grama', 'Agua', 'Fogo'];
+
+function marcarPai(e){
+    let cel = e.parentElement
+    cel.classList.toggle("clicado")
+}
+
+//função que adiciona um valor ao elementoMaquina
+function gerarNumeroRandomico(){
+    return Math.floor(Math.random() * 3);
+}
+
+function gerarElementoMaquina(){
+    elementoMaquina = arrElementos[gerarNumeroRandomico()];
+    return elementoMaquina
+}
+
+//função que pega o texto do elemento clicado
+function marcarEscolha(e){
+    let cel = e.innerText
+    return cel
+}
+
+//loop que adiciona uma função que adiciona o texto do botão clicado
+//na variável escolhaAtual
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener('click', function(){
+        let atual = marcarEscolha(buttons[i])
+        escolhaAtual=atual
+    })
+}
+
+//
+// function telaDeResutado(valor){
+//     if(valor === 'vitoria'){
+//         const vit = document.getElementById('vitoria');
+//         vit.classList.remove('display')
+//     }
+//     if(valor === 'derrota'){
+//         const vit = document.getElementById('derrota');
+//         vit.classList.remove('display')
+//     }
+//     if(valor === 'empate'){
+//         const vit = document.getElementById('empate');
+//         vit.classList.remove('display')
+//     }
+// }
+//
+button.addEventListener("click", function(){
+    let escolhaDoJogador = escolhaAtual
+    let escolhaDaMaquina = gerarElementoMaquina()
+    let ganhouOuNao = ""
+    console.log(escolhaDoJogador)
+    console.log(escolhaDaMaquina)
+    if(escolhaDoJogador===escolhaDaMaquina){
+        ganhouOuNao = 'empate'
+    }
+    if(escolhaDoJogador==="Grama" && escolhaDaMaquina==="Agua"){
+        ganhouOuNao = 'vitoria'
+    }
+    if(escolhaDoJogador==="Grama" && escolhaDaMaquina==="Fogo"){
+        ganhouOuNao = 'derrota'
+    } 
+    if(escolhaDoJogador==="Agua" && escolhaDaMaquina==="Fogo"){
+        ganhouOuNao = 'vitoria'
+    }
+    if(escolhaDoJogador==="Agua" && escolhaDaMaquina==="Grama"){
+        ganhouOuNao = 'derrota'
+    }
+    if(escolhaDoJogador==="Fogo" && escolhaDaMaquina==="Grama"){
+        ganhouOuNao = 'vitoria'
+    }
+    if(escolhaDoJogador==="Fogo" && escolhaDaMaquina==="Agua"){
+        ganhouOuNao = 'derrota'
+    }
+
+    //validação que abre a tela do resultado
+    let valor = ganhouOuNao
+    if(valor === 'vitoria'){
+        const vit = document.getElementById('vitoria');
+        vit.classList.remove('display')
+    }
+    if(valor === 'derrota'){
+        const vit = document.getElementById('derrota');
+        vit.classList.remove('display')
+    }
+    if(valor === 'empate'){
+        const vit = document.getElementById('empate');
+        vit.classList.remove('display')
+    }
+})
+
